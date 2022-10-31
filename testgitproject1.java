3be5c2e5026bcde1f05b1f11af8e9b72a218786e
anuj2077 <110522072+anuj2077@users.noreply.github.com>
2022-10-31 16:00:19 +0530

    Update testgitproject1.java

--- testgitproject1.java
+++ testgitproject1.java
@@ -1,25 +1,4 @@
-node {
-  stage('SCM') {
-    git 'https://github.com/anuj2077/testgitproject1'
-  }
 
-
-stage('Compile-Package'){
-def mvnHome = tool name: 'maven-3', type: 'maven!
-sh "${mvnHome}/bin/mvn package"
-}
-
-
-
-stage('SonarQube Analysis') {
-
-def mvnHome tool name: 'maven-3', type: 'maven'
-
-withSonarQubeEnv('sonar-6') {
-
-sh "${mvnHome}/bin/mvn sonar:sonar"
-}
-}
 class Simple{  
     public static void main(String args[]){  
      System.out.println("Hello Java");  
